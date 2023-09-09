8dc2e3c12611080dcc3634c78c40dab197743985
Idris Yusuf <yusid03@gmail.com>
2023-09-09 12:23:19 +0100

    script update

--- index.js
+++ index.js
@@ -1,30 +1,21 @@
-const daysOfWeek = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
+const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 const getCurrentDay = () => {
   const currentDate = new Date();
-  const options = { weekday: "long", timeZone: "Africa/Lagos" };
-  return new Intl.DateTimeFormat("en-US", options).format(currentDate);
+  return daysOfWeek[currentDate.getDay()];
 };
 
 const updateCurrentTime = () => {
   const currentDate = new Date();
-  const options = { timeZone: "Africa/Lagos", hour12: false };
-  const currentTime = currentDate.toLocaleTimeString("en-US", options);
-  document.querySelector("[data-testid='currentUTCTime']").textContent =
-    currentTime;
+  const currentTime = currentDate.toUTCString();
+  const currentDayElement = document.querySelector("[data-testid='currentDayOfTheWeek']");
+  const currentUTCTimeElement = document.querySelector("[data-testid='currentUTCTime']");
+  
+  currentDayElement.innerHTML = `<strong>Current Day:</strong> ${getCurrentDay()}`;
+  currentUTCTimeElement.innerHTML = `<strong>Current UTC Time:</strong> ${currentTime}`;
 };
 
 document.addEventListener("DOMContentLoaded", () => {
-  document.querySelector("[data-testid='currentDayOfTheWeek']").textContent =
-    getCurrentDay();
   updateCurrentTime();
   setInterval(updateCurrentTime, 1000);
 });
